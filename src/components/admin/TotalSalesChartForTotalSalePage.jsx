1d98021f727baf002999c7aff17721dbd12741f3
JohnRox00 <claudevanroque07@gmail.com>
2023-10-04 02:58:58 +0800

    configure vercel.js

--- src/components/admin/TotalSalesChartForTotalSalePage.jsx
+++ src/components/admin/TotalSalesChartForTotalSalePage.jsx
@@ -29,7 +29,7 @@ const TotalSalesChartForTotalSalePage = ({ dataSales }) => {
         <ArgumentScale factory={scaleBand} />
         <ArgumentAxis />
         <ValueAxis />
-        <BarSeries valueField="totalSales" argumentField="day" />
+        <BarSeries valueField="totalSales" argumentField="month" />
         <Animation />
         <EventTracker />
         <Tooltip />
