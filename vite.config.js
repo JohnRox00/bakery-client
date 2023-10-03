601fa975442c950c4996ad794f164504943aa396
JohnRox00 <claudevanroque07@gmail.com>
2023-10-03 12:38:18 +0800

    third commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: [/^node:.*/],
+      external: [/^node_modules*/],
     },
   },
   plugins: [react()],
